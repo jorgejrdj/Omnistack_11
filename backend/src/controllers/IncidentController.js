dc0a4563ed44a1f50e0e12f110b38cc2b1e58ae6
Jorge de Souza <jorgejrdj1@gmail.com>
2020-03-26 17:51:45 -0300

    Altered incidentController

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -3,20 +3,22 @@ const connection = require('../database/connection');
 module.exports = {
   async index(request, response) {
     const { page = 1 } = request.query;
-    const [count] = await connection('incidents')
-      .count()
+
+    const [count] = await connection('incidents').count();
+    console.log(count);
 
     const incidents = await connection('incidents')
-      .join('ongs', 'ongs_id', '=', 'incidents.ong_id')
+      .join('ongs', 'ong_id', '=', 'incidents.ong_id')
       .limit(5)
       .offset((page - 1) * 5)
       .select([
         'incidents.*',
         'ongs.name',
         'ongs.email',
-        'ong.whatsapp',
+        'ongs.whatsapp',
         'ongs.city',
-        'ongs.uf']);
+        'ongs.uf'
+      ]);
 
     response.header('X-Total-Count', count['count(*)']);
 
@@ -34,7 +36,7 @@ module.exports = {
       ong_id,
     });
 
-    return response.json({ id })
+    return response.json({ id });
   },
 
   async delete(request, response) {
@@ -45,11 +47,9 @@ module.exports = {
       .where('id', id)
       .select('ong_id')
       .first();
-
-    if (incident.ong_id !== ong_id) {
-      return response.status(401).json({ error: 'Operation not permitted.' })
+    if (incident.ong_id != ong_id) {
+      return response.status(401).json({ error: 'Operation not permitted.' });
     }
-
     await connection('incidents').where('id', id).delete();
 
     return response.status(204).send();
