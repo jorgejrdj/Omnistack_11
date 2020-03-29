60910756e0ecde89948674cd00447cb41898466e
Jorge de Souza <jorgejrdj@github.com>
2020-03-29 00:10:24 -0300

    route-config

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 
 import Logon from './pages/Logon';
+import Register from './pages/Register';
 
-export default function Reoutes() {
+export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Logon} />
+        <Route path="/" exact component={Logon} />
+        <Route path="/Register" component={Register} />
       </Switch>
     </BrowserRouter>
   )
